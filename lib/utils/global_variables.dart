4074163fda6620fc9143677faa7a5b00baf5a31a
Matthew Maghakian <99614548+mmaghakian@users.noreply.github.com>
2023-03-06 11:40:43 -0500

    Adding Profile Update Screen

--- lib/utils/global_variables.dart
+++ lib/utils/global_variables.dart
@@ -2,14 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:seneca_social/screens/add_post_screen.dart';
 import 'package:seneca_social/screens/home_screen.dart';
 import 'package:seneca_social/screens/new_post_screen.dart';
+import 'package:seneca_social/screens/profile_screen.dart';
 
 const webScreenSize = 600;
 
 List<Widget> homeScreenItems = [
   const HomeScreen(),
   const Text("Search Screen!"),
-  //const NewPostScreen(),
   const AddPostScreen(),
   const Text("Activity Screen!"),
-  const Text("Profile Screen!"),
+  const ProfileScreen()
+ // const ProfileScreen(),
 ];
