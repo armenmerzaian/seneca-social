2afa5d803555dc15f337aef17173a8ab8934af41
armenmerzaian <71707172+armenmerzaian@users.noreply.github.com>
2023-03-19 18:49:46 -0400

    search screen working

---
+++ lib/screens/search_screen.dart
@@ -0,0 +1,101 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class SearchUsers extends StatefulWidget {
+  const SearchUsers({super.key});
+
+  @override
+  State<SearchUsers> createState() => _SearchUsersState();
+}
+
+class _SearchUsersState extends State<SearchUsers> {
+
+  String searchString = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            title: Card(
+          child: TextField(
+            decoration: const InputDecoration(
+                prefixIcon: Icon(Icons.search), hintText: 'Search...'),
+            onChanged: (val) {
+              setState(() {
+                searchString = val;
+              });
+            },
+          ),
+        )),
+        body: StreamBuilder<QuerySnapshot>(
+          stream: FirebaseFirestore.instance.collection('users').snapshots(),
+          builder: (context, snapshots) {
+            return (snapshots.connectionState == ConnectionState.waiting)
+                ? const Center(
+                    child: CircularProgressIndicator(),
+                  )
+                : ListView.builder(
+                    itemCount: snapshots.data!.docs.length,
+                    itemBuilder: (context, index) {
+                      var data = snapshots.data!.docs[index].data()
+                          as Map<String, dynamic>;
+
+                      if (searchString.isEmpty) {
+                        return ListTile(
+                          title: Text(
+                            data['username'],
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                            style: const TextStyle(
+                                color: Colors.black54,
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          subtitle: Text(
+                            data['email'],
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                            style: const TextStyle(
+                                color: Colors.black54,
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          leading: CircleAvatar(
+                            backgroundImage: NetworkImage(data['profileImgUrl']),
+                          ),
+                        );
+                      }
+                      if (data['username']
+                          .toString()
+                          .toLowerCase()
+                          .startsWith(searchString.toLowerCase())) {
+                        return ListTile(
+                          title: Text(
+                            data['username'],
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                            style: const TextStyle(
+                                color: Colors.black54,
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          subtitle: Text(
+                            data['email'],
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                            style: const TextStyle(
+                                color: Colors.black54,
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          leading: CircleAvatar(
+                            backgroundImage: NetworkImage(data['profileImgUrl']),
+                          ),
+                        );
+                      }
+                      return Container();
+                    });
+          },
+        ));
+  }
+}
--- lib/utils/global_variables.dart
+++ lib/utils/global_variables.dart
@@ -2,12 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:seneca_social/screens/add_post_screen.dart';
 import 'package:seneca_social/screens/home_screen.dart';
 import 'package:seneca_social/screens/new_post_screen.dart';
+import 'package:seneca_social/screens/search_screen.dart';
 
 const webScreenSize = 600;
 
 List<Widget> homeScreenItems = [
   const HomeScreen(),
-  const Text("Search Screen!"),
+  const SearchUsers(),
   //const NewPostScreen(),
   const AddPostScreen(),
   const Text("Activity Screen!"),
